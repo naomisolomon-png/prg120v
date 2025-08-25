e4433be60ebcc336147e2922fce671991fe58a63
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-08-25 16:23:09 +0200

    La inn ny oppgave

---
+++ Tema1/oppgave2.php
@@ -0,0 +1,9 @@
+<?php
+/* Programmet mottar fra et HTML-skjema et fornavn, et etternavn og alder ved POST-metoden
+/* Programmet skriver ut en "god dag"-melding med personens navn og alder
+*/
+$fornavn=$_POST ["fornavn"];
+$etternavn=$_POST ["etternavn"]; /
+$alder=$_POST ["alder"]; / * variable gitt verdier fra feltene i HTML-skjemaet */
+print ("God dag $fornavn $etternavn $alder år <br />"); /* resultat skrevet ut */
+?>
