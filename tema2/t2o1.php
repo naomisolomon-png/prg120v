9a95625659e767b128b9b682b2704e000a4e7282
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-10 15:38:11 +0200

    Changed a mistake

--- tema2/t2o1.php
+++ tema2/t2o1.php
@@ -2,13 +2,17 @@
 /* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3?"
 /* Programmet sjekker om svaret er riktig og skriver ut en avhengig av om svaret er riktig eller galt
 */
-$svar=$_POST ["svar"];
+if (isset($_POST["svar"])) /* Svar-knappen er trykket */
+{
+    $svar = $_POST["svar"];
 if ($svar == 9)  /* Angitt svar er riktig */
 {
+
     print ("Riktig. 3 ganger 3 er 9! <br/>");
 }
 else  /* Angitt svar er galt */
 {
     print ("Feil. 3 ganger 3 er 9! <br/>");
+}
 }
 ?>
