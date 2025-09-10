06560109d20cc122b539ade0c278fd08ca5bee38
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-10 15:28:15 +0200

    Changes mistakes

--- tema2/t2o8.php
+++ tema2/t2o8.php
@@ -15,13 +15,15 @@
 </html>
 
 <?php     /* Tema 2 Oppgave 1 */
-/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3?"
-/* Programmet sjekker om svaret er riktig og skriver ut en avhengig av om svaret er riktig eller galt
-*/
+/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3?" */
+/* Programmet sjekker om svaret er riktig og skriver ut en avhengig av om svaret er riktig eller galt */
+
 if (isset($_POST["svar"])) /* Svar-knappen er trykket */
-$svar=$_POST ["svar"];
-if ($svar == 9)  /* Angitt svar er riktig */
 {
+    $svar = $_POST["svar"];
+if ($svar == 9)  /* Angitt svar er riktig */
+
+
     print ("Riktig. 3 ganger 3 er 9! <br/>");
 }
 else  /* Angitt svar er galt */
