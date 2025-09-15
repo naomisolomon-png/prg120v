16333155828ea23d80d3c9f2913e36c60332e946
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-15 16:15:31 +0200

    added files

---
+++ tema3/t3o2.php
@@ -0,0 +1,10 @@
+<?php /* Oppgave 2 */
+/*
+/* Programmet skriver ut tallene fra 1 til 10 og kvadratene av disse
+*/
+for ($tall=1;$tall<=10;$tall++) /* repetisjon fra 1 til 10 */
+{
+$kvadrat=$tall*$tall; /* kvadratet beregnet */
+print("$tall har kvadratet $kvadrat <br/>"); /* tallet og kvadratet skrevet ut */
+}
+?>
