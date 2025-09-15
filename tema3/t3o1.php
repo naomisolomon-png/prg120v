61c4e50aa89e3cd657ac384cfe279b56cb555b7f
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-15 16:09:54 +0200

    added new files and maps

---
+++ tema3/t3o1.php
@@ -0,0 +1,9 @@
+<?php /* Oppgave 1 */
+/*
+/* Programmet skriver ut tallene fra 1 til 10 på hver sin linje
+*/
+for ($tall=1;$tall<=10;$tall++) /* repetisjon fra 1 til 10 */
+{
+print("$tall <br/>"); /* tallet skrevet ut */
+}
+?>
