59b4e1081e0d1d93a345e0948ec8d90419443c06
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-29 14:24:59 +0200

    changed file

--- tema5og6/1-3-statisk-listeboks-med-tom-rad.php
+++ tema5og6/1-3-statisk-listeboks-med-tom-rad.php
@@ -1,33 +1,35 @@
-<?php /* statistisk listeboks med valgt rad */
-/* Programmet lager et skjema med en statistisk listeboks med ukedag der første rad er tom */
-?>
+<?php  /* statisk-listeboks */
+/*
+/*  Programmet lager et skjema med en statisk listeboks med ukedag der første rad er tom
+*/
+?> 
 
-<h3> Velg ukedag </h3>
+<h3>Velg ukedag</h3>
 
 <form method="post" action="" id="velgUkedagSkjema" name="velgUkedagSkjema">
-    Ukedag
-    <select name="ukedag" id="ukedag">
-        <option value="">velg ukedag</option>
-        <option value="mandag">mandag</option>
-        <option value="tirsdag">tirsdag</option>
-        <option value="onsdag">onsdag</option>
-        <option value="torsdag">torsdag</option>
-        <option value="fredag">fredag</option>
-    </select> <br />
-    <input type="sumbit" value="Velg ukedag" id="velgUkedagKnapp" name="velgUkedagKnapp" />
+  Ukedag
+  <select name="ukedag" id="ukedag">
+    <option value="">velg ukedag</option>
+    <option value="mandag">mandag</option>
+    <option value="tirsdag">tirsdag</option>
+    <option value="onsdag">onsdag</option>
+    <option value="torsdag">torsdag</option>
+    <option value="fredag">fredag</option>
+  </select>  <br />
+  <input type="submit" value="Velg ukedag" id="velgUkedagKnapp" name="velgUkedagKnapp" /> 
 </form>
 
 <?php
-if (isset($_POST ["velgUkedagKnapp"]))
-{
-    $ukedag=$_POST ["ukedag"];
-    if (!$ukedag)
+  if (isset($_POST ["velgUkedagKnapp"]))
     {
-        print ("Ukedag er ikke valgt <br />");
+      $ukedag=$_POST ["ukedag"];
+      if (!$ukedag)
+        {
+          print ("Ukedag ikke valgt <br />");
+        }
+      else
+        {
+          print ("F&oslash;lgende ukedag er valgt: $ukedag <br />");
+        }	
     }
-    else
-    {
-        print ("Foslash;lgende ukedag er valgt: $ukedag </br />");
-    }
-}
-?>
+?> 
