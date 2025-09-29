a936c57fde4c0f6b72fea096f13796aef5604890
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-29 14:36:27 +0200

    edited mistake

--- tema5og6/dynamiske-funksjoner.php
+++ tema5og6/dynamiske-funksjoner.php
@@ -39,21 +39,25 @@ print("<option value='$emnekode'>$emnekode $emnenavn </option>"); /* ny verdi i 
 }
 function listeboksPostnr()
 {
-    include("db-tilkobling.php"); /* tilkobling til database-server og valg av database utført */
-    $sqlSetning="SELECT * FROM poststed ORDER BY postnr;";
-    $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+  include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
+      
+  $sqlSetning="SELECT * FROM poststed ORDER BY postnr;";
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen"); 
     /* SQL-setning sendt til database-serveren */
-    $antallRader=mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
-    for ($r=1;$r<=$antallRader;$r++)
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  for ($r=1;$r<=$antallRader;$r++)
     {
-        $rad=mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-        $postnr=$rad["postnr"];
-        $poststed=$rad["poststed"];
-        
-        print("<option value='$postnr'>$postnr $poststed</option>"); /* ny verdi i listeboksen laget */
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra spørringsresultatet */
+      $postnr=$rad["postnr"]; 
+      $poststed=$rad["poststed"];
+
+      print("<option value='$postnr'>$postnr $poststed</option>");  /* ny verdi i listeboksen laget */
     }
-    }
-   function sjekkbokserPostnr()
+}
+
+function sjekkbokserPostnr()
 {
   include("db-tilkobling.php");  /* tilkobling til database-server og valg av database utført */
       
