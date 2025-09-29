761dc29a39e84d4d6ad7507e9eee5c0ae72e422c
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-29 14:14:28 +0200

    edited file

--- tema5og6/1-2-statisk-listeboks-med-valgt-rad.php
+++ tema5og6/1-2-statisk-listeboks-med-valgt-rad.php
@@ -1,25 +1,27 @@
-<?php /* statistisk listeboks med valgt rad */
-/* Programmet lager et skjema med en statistisk listeboks med ukedag der en rad er valgt */
-?>
+<?php  /* statisk-listeboks */
+/*
+/*  Programmet lager et skjema med en statisk listeboks med ukedag der en rad er valgt
+*/
+?> 
 
-<h3> Velg ukedag </h3>
+<h3>Velg ukedag</h3>
 
 <form method="post" action="" id="velgPoststedSkjema" name="velgPoststedSkjema">
-    Ukedag
+  Ukedag
+  <select name="ukedag" id="ukedag" >
     <option value="mandag">mandag</option>
     <option value="tirsdag">tirsdag</option>
     <option value="onsdag" selected>onsdag</option>
     <option value="torsdag">torsdag</option>
     <option value="fredag">fredag</option>
-</select> <br />
-<input type="submit" value="Velg ukedag" id="velgUkedagKnapp" name="velgUkedagKnapp" />
+  </select>  <br />
+  <input type="submit" value="Velg ukedag" id="velgUkedagKnapp" name="velgUkedagKnapp" /> 
 </form>
 
 <?php
-if (isset($_POST ["velgUkedagKnapp"]))
-{
-    $ukedag=$_POST ["ukedag"];
-    print ("F&oslash;lgende ukedag er valgt: $ukedag <br />");
-}
-?>
-
+  if (isset($_POST ["velgUkedagKnapp"]))
+    {
+      $ukedag=$_POST ["ukedag"];
+      print ("F&oslash;lgende ukedag er valgt: $ukedag <br />");
+    }
+?> 
