12f4fc73ef57fee15249006a2294a722c4dcb88d
naomisolomon-png <naomiamaliesolomon@gmail.com>
2025-09-29 14:38:48 +0200

    edited files

--- tema5og6/db-tilkobling.php
+++ tema5og6/db-tilkobling.php
@@ -1,11 +1,12 @@
-<?php /* db-tilkobling */
+<?php  /* db-tilkobling */
 /*
-/* Programmet foretar tilkobling til database-server og valg av database
+/*  Programmet foretar tilkobling til database-server og valg av database
 */
 $host = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $database = getenv('DB_DATABASE');
-$db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
-/* tilkobling til database-serveren utført */
-?>
+
+ $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+    /* tilkobling til database-serveren utført */
+ 
