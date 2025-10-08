ace0e89093a8a24c5d23b31c3dc2cd877fed29a6
naomisolomon-png <namel9439@usn.no>
2025-10-08 15:18:07 +0200

    .

--- oblig2/registrer-student.php
+++ oblig2/registrer-student.php
@@ -23,7 +23,7 @@ $brukernavn=$_POST ["brukernavn"];
 $fornavn=$_POST ["fornavn"];
 $etternavn=$_POST ["etternavn"];
 $klassekode=$_POST ["klassekode"];
-if (!$brukernavn || !$fornavn || !$etternavn || ! !$klassekode)
+if (!$brukernavn || !$fornavn || !$etternavn || !$klassekode)
 {
 print ("Alle felt m&aring; fylles ut");
 }
