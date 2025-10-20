4b5ca1d5c17c65b6a5c1a2ee326e509d7e2e359f
naomisolomon-png <namel9439@usn.no>
2025-10-20 09:52:46 +0200

    .

--- slett-klasse.php
+++ slett-klasse.php
@@ -1,6 +1,7 @@
 <?php  /* slett-klasse */
 /*
-/*  Programmet lager et skjema for å velge en eller flere klasser som skal slettes  
+/*  Programmet lager et skjema for å velge en eller flere klasser som skal slettes 
+/* Programmet sjekker om det er studenter i klassen før sletting 
 /*  Programmet sletter de valgte klassene */
 ?>
 
@@ -9,30 +10,44 @@
 <h3>Slett klasse</h3>
 
 <form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onSubmit="return bekreft()">
-  Student <br />
+  Klasse <br />
   <?php include("dynamiske-funksjoner.php"); sjekkbokserKlasse(); ?> <br/>
-  <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" /> 
+  <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
 </form>
 
 <?php
-  if (isset($_POST ["slettKlasseKnapp"]))
+  if (isset($_POST["slettKlasseKnapp"]))
     {
-      @$klassekode=$_POST ["klassekode"];
-      $antall=count($klassekode);
+      @$klassekode = $_POST["klassekode"];
+      $antall = is_array($klassekode) ? count($klassekode) : 0;
 
-      if ($antall==0)
+      if ($antall == 0)
         {
-          print ("Ingen klasser ble valgt <br />");
+          print ("Ingen klasser er valgt. <br />");
         }
       else
         {
-          include("db-tilkobling.php");  	
-          for ($r=0;$r<$antall;$r++)
+          include("db-tilkobling.php");
+          for ($r = 0; $r < $antall; $r++)
             {
-              $sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode[$r]';";
-              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+              $kk = mysqli_real_escape_string($db, $klassekode[$r]);
+
+              /* Sjekk om det finnes studenter i klassen */
+              $sjekkSql = "SELECT COUNT(*) AS cnt FROM klasse WHERE klassekode = '$kk';";
+              $sjekkResult = mysqli_query($db, $sjekkSql) or die("Feil ved sjekk i databasen");
+              $row = mysqli_fetch_assoc($sjekkResult);
+
+              if ($row['cnt'] > 0)
+                {
+                  print ("Du kan ikke slette en klasse som har studenter i seg (klassekode: $kk) <br />");
+                }
+              else
+                {
+                  $sqlSetning = "DELETE FROM klasse WHERE klassekode='$kk';";
+                  mysqli_query($db, $sqlSetning) or die ("Ikke mulig å slette data i databasen");
+                }
             }
-          print ("Valgte klasse eller klasser er n&aring; slettet <br />");
+          print ("Operasjonen er ferdig <br />");
         }
     }
-?> 
+?>
